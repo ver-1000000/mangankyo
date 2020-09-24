8035eee4bfb4c26726981cf086a085189ece6489
ver-1000000 <ver-1000000@users.noreply.github.com>
2020-09-24 15:45:43 +0000

    deploy: cef802b6aed44011b822e5ed5df7c61439152753

---
+++ precache-manifest.1ad62e5f80cf9add61fbb4017922107d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021333aa00843f415293333d5533820f",
+    "url": "/mangankyo/index.html"
+  },
+  {
+    "revision": "5c743fc9ef80037411bf",
+    "url": "/mangankyo/static/css/2.a5c636dd.chunk.css"
+  },
+  {
+    "revision": "0c407aeabefde82b8821",
+    "url": "/mangankyo/static/css/main.5dca9d97.chunk.css"
+  },
+  {
+    "revision": "5c743fc9ef80037411bf",
+    "url": "/mangankyo/static/js/2.d1a01107.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mangankyo/static/js/2.d1a01107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c407aeabefde82b8821",
+    "url": "/mangankyo/static/js/main.6da2fbd7.chunk.js"
+  },
+  {
+    "revision": "eabe6fe4c748dca3cbd0",
+    "url": "/mangankyo/static/js/runtime-main.9d0c08a3.js"
+  }
+]);
